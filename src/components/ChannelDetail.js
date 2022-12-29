44ede9f980a8fe12a4cd9f9c5758611457bb4692
Kevin Everrett <everrett.developer@gmail.com>
2022-12-29 17:11:18 -0500

    ChannelDetail

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import { fetchApi } from '../utils/fetchApi';
+import ChannelCard from './ChannelCard';
+import Videos from './Videos';
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  console.log(channelDetail);
+
+  useEffect(() => {
+    fetchApi(`channels?part="snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchApi(`search?channelId=${id}&part=snippet&order=data`).then((data) =>
+      setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            height: '300px',
+            background:
+              'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+            zIndex: 10,
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
